f6914b055580a2305b9a864327e926b008260d34
Jaee <elavl@naver.com>
2025-08-08 02:31:07 +0900

    Title: 최단 Run Length 인코딩, Time: 118ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250808/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 250808/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -1,7 +1,10 @@
 import java.util.*;
+import java.util.Map.Entry;
+
 
 public class Main {
     static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String A = sc.next();
@@ -16,19 +19,29 @@ public class Main {
             }
             arr[0] = lastAlpha;
 
-            int cnt=1;
-    
+
+            Map<Character,Integer> result = new HashMap<>();
+            result.put(arr[0],1);
+
             for(int k=1; k<arr.length; k++){
                 if(arr[k] != arr[k-1]){
+                    result.put(arr[k],1);
+                }else{
+                    int cnt = result.get(arr[k]);
+                    result.put(arr[k],cnt+1);
+                }
+            }
+            int cnt =0;
+            for(Entry<Character, Integer> encodingMap :result.entrySet()){
+                if(encodingMap.getValue()>=10){
+                    cnt=+2;
+                }else{
                     cnt++;
                 }
             }
-            if(cnt==1){
-                min = Math.min(min, 3);
-            }else{
-                min = Math.min(cnt*2, min);
+            cnt+=result.size();
 
-            }
+            min = Math.min(cnt,min);
         
         }
         System.out.println(min);
