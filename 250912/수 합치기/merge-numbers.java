95d7a1270f13420ff92e1f2c40e38599b45464ba
Jaee <elavl@naver.com>
2025-09-12 23:49:25 +0900

    Title: 수 합치기, Time: 948ms, Memory: 37MB, Status: Passed - Codetree

--- 250912/수 합치기/merge-numbers.java
+++ 250912/수 합치기/merge-numbers.java
@@ -1,30 +1,31 @@
 import java.util.*;
 
 public class Main {
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+            return Integer.compare(a,b);
+        });
+
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            pq.offer(sc.nextInt());
         }
         // Please write your code here.
-
         int sum = 0;
-        while(true){
-            Arrays.sort(arr);
-
-            if(arr[1]==Integer.MAX_VALUE){
-                System.out.println(sum);
-
-                return;
-            }else{
-                sum+=(arr[0]+arr[1]);
-                arr[0]=(arr[0]+arr[1]);
-                arr[1]=Integer.MAX_VALUE;
-            }
-
+        while(pq.size()>1){
+            int num1= pq.poll();
+            int num2 = pq.poll();
+            sum+=(num1+num2);
+            pq.offer(num1+num2);
+            
         }
+        System.out.println(sum);
+
+
+        
         
 
     }
