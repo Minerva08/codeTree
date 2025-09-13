ea4c5544172645c4a3a2f12b2e189f9d4908a5d8
Jaee <elavl@naver.com>
2025-09-13 20:00:43 +0900

    Title: 선분 위의 점, Time: 504ms, Memory: 24MB, Status: Wrong Answer - Codetree

---
+++ 250913/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Main {
+
+    // 상한 + 하한 으로 범위 찾기
+    // 범위 내 해당하는 점의 개수 찾기
+    static int n;
+    static int[] points;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        points = new int[n];
+        for (int i = 0; i < n; i++) {
+            points[i] = sc.nextInt();
+        }
+        int a, b;
+        for (int i = 0; i < m; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            // 이상의 시작 범위
+            int lowIdx = lowerBound(Math.min(a,b));
+            // 초과의 시작 범위
+            int upBound = upperBound(Math.max(a,b));
+
+            System.out.println(upBound-lowIdx);
+        }
+        
+        // Please write your code here.
+
+        
+    }
+
+    public static int lowerBound(int target){
+        int left = 0;
+        int right = n;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(points[mid]>=target){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left;
+
+    }
+
+    public static int upperBound(int target){
+        int left = 0;
+        int right = n;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(points[mid]<=target){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+
+    }
+}
