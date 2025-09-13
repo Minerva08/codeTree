fdbcfecdaea04d446e62172d1b1b7d629ade884e
Jaee <elavl@naver.com>
2025-09-13 19:44:03 +0900

    Title: 가장 먼저 나오는 숫자, Time: 1578ms, Memory: 52MB, Status: Passed - Codetree

--- 250913/가장 먼저 나오는 숫자/first-appear-number.java
+++ 250913/가장 먼저 나오는 숫자/first-appear-number.java
@@ -24,24 +24,25 @@ public class Main {
             int target = queries[i];
             // 하한값/상한값은 범위이므로 일치하지 않아도됨
             //left<right
+            // <= 가 아니라 < 를 쓰는 이유는 mid를 right = mid 로 줄여나가기 때문 )
             int left = 0;
-            int right = n;
-            int minIdx= -1;
+            int right = n-1;
+            // int minIdx= -1;
             while(left<right){
                 int mid = (left+right)/2;
 
                 if(target<=arr[mid]){
                     //정확히 일치하는 값의 위치 중 최소를 찾아야 하므로
-                    if(target==arr[mid]){
-                        minIdx=mid;
-                    }
+                    // if(target==arr[mid]){
+                    //     minIdx=mid;
+                    // }
                     right = mid;
                 }else{
                     left = mid+1;
                 }
                 
             }
-            System.out.println(minIdx==-1? -1: (minIdx+1));
+            System.out.println((left<n) && (arr[left]==target)? (left+1):-1);
         }
     }
 }
