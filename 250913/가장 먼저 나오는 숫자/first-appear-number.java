ac8536bfccf19400d28ee10d2bcc193cc3b8b62a
Jaee <elavl@naver.com>
2025-09-13 15:06:24 +0900

    Title: 가장 먼저 나오는 숫자, Time: 896ms, Memory: 33MB, Status: Wrong Answer - Codetree

---
+++ 250913/가장 먼저 나오는 숫자/first-appear-number.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] queries = new int[m];
+        for (int i = 0; i < m; i++) {
+            queries[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        int[] result = new int[m];
+
+        for(int i=0; i<m; i++){
+            int target = queries[i];
+
+            int left=0;
+            int right = n-1;
+            int minIdx=n-1;
+
+            while(left<=right){
+                int mid = (left+right)/2;
+
+                if(target<=arr[mid]){
+                    if(target==arr[mid]){
+                       minIdx = Math.min(minIdx,mid);
+                    }  
+                    right = mid-1;
+
+                }else{
+                    // target>mid
+                    left= mid+1;
+                }
+
+            }
+
+            if(minIdx==(n-1)){
+                result[i]=-1;
+            }else{
+                result[i] = minIdx+1;
+
+            }
+
+
+        }
+
+
+        for(int i=0; i<m; i++){
+            System.out.println(result[i]);
+        }
+        
+    }
+}
